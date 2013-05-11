ebf0ac70fa5b2e5c80e8e05f73b72d4aacf7edad
tylermorgan86 <tylermorgan86@gmail.com>
2013-05-10 21:59:46 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkBlog::Application.config.secret_token = '075ab7028d73fe68b226a472887a535d28c78f41ee7673a1765191f699e4cafdea6e62e272cf4e99745b7edbe7f1b38272f1f186b30c8f30c698b9194dba9a92'
